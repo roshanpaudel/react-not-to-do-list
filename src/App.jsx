0d2fdfdadcb1706480f33e87aa478bd514ab9929
roshanpaudel <paudelroshan93@gmail.com>
2025-05-22 20:08:49 +1000

    Bugs fixed on app.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useRef, useState } from "react";
-import "./App.css";
 import { UserInput } from "./UserInput";
 import { Table } from "./Table";
 import { HoursBanner } from "./HoursBanner";
